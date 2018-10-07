534fa3f788ccab5c7369a0ed3bd0c8a57ec8f55a
Vitor Costa <vitu86@gmail.com>
2018-10-07 16:38:09 -0300

    Fixing meme model, now is a struct

--- MemeMe/MemeMe/Model/Meme.swift
+++ MemeMe/MemeMe/Model/Meme.swift
@@ -9,18 +9,10 @@
 import Foundation
 import UIKit
 
-class Meme {
-    
-    // MARK: Public properties
+
+struct Meme {
     var topText:String
     var bottomText:String
     var originalImage:UIImage
     var memedImage:UIImage
-    
-    init(topText:String, bottomText:String, originalImage:UIImage, memedImage:UIImage){
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-    }
 }
